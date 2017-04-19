e92de8570986b0dcd5b048312aa20295ad488a28
Othuv <sean4gaming@yahoo.co.uk>
2017-04-19 14:20:27 +0100

    AI-V_0.0
    In other words nothing just yet

--- include/aitank.h
+++ include/aitank.h
@@ -21,15 +21,15 @@ public:
 	bool isVisible()const {return visible;} //!< Is the tank visiable to the player?
 
 	// FSM public methods, set states and variable
-	virtual void reset()=0; //!< Reset any variables you need to when the tank has been shot
-	virtual void move()=0; //!< Move the tank
-	virtual void collided() = 0; //!< Called by the game object when the tank has collided
-	virtual void markTarget(Position p /** \param Position of the acquired target */) = 0; //!< Called by the game object when a target (enemy building) comes within the tanks visible range
-	virtual void markEnemy(Position p /** \param Position of the enemy tank */) = 0; //!< Called by the game object when the enemy tank comes within the tanks visible range
-	virtual void markBase(Position p /** \param Position of the home building */) = 0; //!< Called by the game object when one of you own buildings comes within the tanks visible range
-	virtual void markShell(Position p /** \param Position of an enemy shell */) = 0; //!< Called by the game object when enemy shell comes within the tanks visible range
-	virtual bool isFiring() = 0; //!< Called by the game object each frame.  When this function returns true (and ammo is availbe and loaded) the tank will fire a shell
-	virtual void score(int thisScore,int enemyScore) = 0;
+	virtual void reset(); //!< Reset any variables you need to when the tank has been shot
+	virtual void move(); //!< Move the tank
+	virtual void collided(); //!< Called by the game object when the tank has collided
+	virtual void markTarget(Position p /** \param Position of the acquired target */); //!< Called by the game object when a target (enemy building) comes within the tanks visible range
+	virtual void markEnemy(Position p /** \param Position of the enemy tank */); //!< Called by the game object when the enemy tank comes within the tanks visible range
+	virtual void markBase(Position p /** \param Position of the home building */); //!< Called by the game object when one of you own buildings comes within the tanks visible range
+	virtual void markShell(Position p /** \param Position of an enemy shell */); //!< Called by the game object when enemy shell comes within the tanks visible range
+	virtual bool isFiring(); //!< Called by the game object each frame.  When this function returns true (and ammo is availbe and loaded) the tank will fire a shell
+	virtual void score(int thisScore,int enemyScore);
 };
 
 #endif
--- src/aitank.cpp
+++ src/aitank.cpp
@@ -1,6 +1,6 @@
 #include "AITank.h"
 
-AITank::AITank() // Construtor
+AITank::AITank() // Constructor
 {
 	bodyTex.loadFromFile("assets\\redTank.png");
 	body.setTexture(bodyTex);
@@ -13,4 +13,49 @@ AITank::AITank() // Construtor
 	turret.setScale(0.2f,0.2f);
 	
 	numberOfShells = 15;
+}
+
+void AITank::reset()
+{
+
+}
+
+void AITank::move()
+{
+
+}
+
+void AITank::collided()
+{
+
+}
+
+void AITank::markTarget(Position p)
+{
+
+}
+
+void AITank::markBase(Position p)
+{
+
+}
+
+void AITank::markEnemy(Position p)
+{
+
+}
+
+void AITank::markShell(Position p)
+{
+
+}
+
+bool AITank::isFiring()
+{
+
+}
+
+void AITank::score(int thisScore, int enemyScore)
+{
+
 }
