bac00a770f6df32bbf95bfabe0cf7eb87c19ac67
Unknown <benmasdin@googlemail.com>
2017-04-21 17:37:58 +0100

    MORE COMMENTS, ALL COMMENTS

--- include/game.h
+++ include/game.h
@@ -22,37 +22,37 @@ class Game : public sf::Drawable
 		vector<vector<bool>> adjacencyMatrix;				// adjacency matrix [nNodes] [nNodes]
 //		vector<vector<Position>> positionMatrix;			// position matrix of size [width] [height], scraped for a calculation
 
-		unsigned short lives; // number of lives left
-		bool debugMode; // toggle for debug mode
-		sf::RectangleShape background; // Background the playing area
-		sf::RectangleShape ammoArea; // Background where the ammo is drawn
+		unsigned short lives;								// number of lives left
+		bool debugMode;										// toggle for debug mode
+		sf::RectangleShape background;						// Background the playing area
+		sf::RectangleShape ammoArea;						// Background where the ammo is drawn
 		list<Position> aiPath;
 		list<int> lIPath;
-		list<Obstacle> obstacles; // Obstacles in the tanks way
-		list<Obstacle> blueBuildings; // Collection of blue buildings
-		list<Obstacle> redBuildings; // Collection of red buildings
-		list<Shell> shells; // Shells fired from tanks
-		void resetNpc(); // Move the NPC after it has been shot
-		void resetPlayer(); // Move the player after it has been shot
-		void fireShell(Position fp, bool npc); // Fire a shell
-		int redScore; // Score of red tank
-		int blueScore; // Score of blue tank
+		list<Obstacle> obstacles;							// Obstacles in the tanks way
+		list<Obstacle> blueBuildings;						// Collection of blue buildings
+		list<Obstacle> redBuildings;						// Collection of red buildings
+		list<Shell> shells;									// Shells fired from tanks
+		void resetNpc();									// Move the NPC after it has been shot
+		void resetPlayer();									// Move the player after it has been shot
+		void fireShell(Position fp, bool npc);				// Fire a shell
+		int redScore;										// Score of red tank
+		int blueScore;										// Score of blue tank
     public:
-		Game(); // Constructor
-		~Game(); // Destructor
-		int chooseGoal();
-		int makeInitialGoal();
-		int makeRandomGoal();
-		int makeEnemyTerritoryGoal();
-		virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const; // Draw the game
-		void play(); // Play the game for one timestep
-		AITank npc = AITank(); // Red tank
-		PlayerTank player; // Blue tank
-		void keyPressed(sf::Keyboard::Key key); // function for processing input
-		void keyReleased(sf::Keyboard::Key key); // function for processing input
-		bool gameOver() const; // Has the game finished?
-		int numBlueBuildings() const; // Count of blue buildings
-		int numRedBuildings() const; // Count of red buildings
+		Game();																					// Constructor
+		~Game();																				// Destructor
+		int chooseGoal();																		// Select what type of path will be taken
+		int makeInitialGoal();																	// Decides the initial position to path find to
+		int makeRandomGoal();																	// Creates a random position to path find to
+		int makeEnemyTerritoryGoal();															// Creates a position in the enemy (the players) territory
+		virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;				// Draw the game
+		void play();																			// Play the game for one timestep
+		AITank npc = AITank();																	// Red tank
+		PlayerTank player;																		// Blue tank
+		void keyPressed(sf::Keyboard::Key key);													// function for processing input
+		void keyReleased(sf::Keyboard::Key key);												// function for processing input
+		bool gameOver() const;																	// Has the game finished?
+		int numBlueBuildings() const;															// Count of blue buildings
+		int numRedBuildings() const;															// Count of red buildings
 
 		//The following are functions that have been added to the game to facilitate pathing
 		int index(int x, int y);								//return the index for a pair of coords
